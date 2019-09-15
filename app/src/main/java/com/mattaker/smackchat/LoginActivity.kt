4292940d66e144a4ad081392a7cd88b33442a838
akerMatt <matthew_aker@outlook.com>
2019-09-14 22:31:05 -0400

    Initial commit

---
+++ app/src/main/java/com/mattaker/smackchat/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.mattaker.smackchat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun btnLogin_loginClicked(view: View){
+
+    }
+
+    fun btnCreateAcc_loginClicked(view: View){
+
+    }
+}
